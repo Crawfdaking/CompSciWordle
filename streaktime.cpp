2bf120ed7b5a627cd415dee783db91f30c80ee2b
TheChiro <116473048+TheChiro@users.noreply.github.com>
2022-12-06 21:03:02 -0800

    Create streaktime.cpp

---
+++ streaktime.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+void time(){
+	 auto clock_end = std::chrono::system_clock::now();
+	    std::time_t clock_end_time = std::chrono::system_clock::to_time_t(clock_end);
+	    std::cout << "The current system date and time is: " << std::ctime(&clock_end_time)<< std::endl;
+}
+void playagain(){
+	cout<<"you can play again after midnight ;)"<<endl;
+	break;
+}
+void streak(){
+  rightguess=answer;
+  int LongerStreak = 0, CurrentStreak = 0;
+if(answer == result)
+    CurrentStreak++;
+else
+    CurrentStreak = 0;
+if(CurrentStreak > LongerStreak)
+    LongerStreak = CurrentStreak;
+}
