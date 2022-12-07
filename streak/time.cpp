b137de47fa0953722deb58e11af496638de9b68e
TheChiro <116473048+TheChiro@users.noreply.github.com>
2022-12-06 20:59:40 -0800

    Update time.cpp

--- streak/time.cpp
+++ streak/time.cpp
@@ -1,4 +1,4 @@
-#using namespace std;
+using namespace std;
 void streak(){
 int LongerStreak = 0, CurrentStreak = 0;
 	if(answer == result)
@@ -7,4 +7,13 @@ int LongerStreak = 0, CurrentStreak = 0;
 	    CurrentStreak = 0;
 	if(CurrentStreak > LongerStreak)
 	    LongerStreak = CurrentStreak;
+}
+void time(){
+	 auto clock_end = std::chrono::system_clock::now();
+	    std::time_t clock_end_time = std::chrono::system_clock::to_time_t(clock_end);
+	    std::cout << "The current system date and time is: " << std::ctime(&clock_end_time)<< std::endl;
+}
+void playagain(){
+	cout<<"you can play again after midnight"<<endl;
+	break;
 }
